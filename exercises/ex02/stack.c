1c4ba9fa7718342729bf00da080028fc6672abbf
Allen Downey <downey@allendowney.com>
2016-12-21 13:19:19 -0500

    Adding exercises 1 and 2

---
+++ exercises/ex02/stack.c
@@ -0,0 +1,47 @@
+/* Example code for Think OS.
+
+Copyright 2014 Allen Downey
+License: GNU GPLv3
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 5
+
+int *foo() {
+    int i;
+    int array[SIZE];
+
+    //printf("%p\n", array);
+
+    for (i=0; i<SIZE; i++) {
+	array[i] = 42;
+    }
+    return array;
+}
+
+void bar() {
+    int i;
+    int array[SIZE];
+
+    //printf("%p\n", array);
+
+    for (i=0; i<SIZE; i++) {
+	array[i] = i;
+    }
+}
+
+int main()
+{
+    int i;
+    int *array = foo();
+    bar();
+
+    for (i=0; i<SIZE; i++) {
+	printf("%d\n", array[i]);
+    }
+
+    return 0;
+}

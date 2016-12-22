b6526c93af7b66e634339d57a58dfaadef73c37b
Allen Downey <downey@allendowney.com>
2016-12-22 15:03:18 -0500

    Adding ex08

---
+++ exercises/ex08/ex_compile.c
@@ -0,0 +1,17 @@
+/* Simple example using Glib.
+
+From http://www.ibm.com/developerworks/linux/tutorials/l-glib/
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
+
+int main(int argc, char** argv) {
+    GList* list = NULL;
+    list = g_list_append(list, "Hello world!");
+    printf("The first item is '%s'\n", (char *) g_list_first(list)->data);
+    return 0;
+}
+

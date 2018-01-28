f65e3fe786f6afc3ce28048bfe381c978dc62814
isa <isabel.blancett@students.olin.edu>
2018-01-28 17:32:10 -0500

    Compiling cards example from HFC

---
+++ exercises/ex01/cards.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+  char card_name[3];
+  int count = 0;
+  while (card_name[0] != 'X') {
+
+    puts("Enter the card_name: ");
+    scanf("%2s", card_name);
+    int val = 0;
+
+    switch(card_name[0]) {
+
+    case 'K':
+    case 'Q':
+    case 'J':
+      val = 10;
+      break;
+
+    case 'A':
+      val = 11;
+      break;
+
+    case 'X':
+      continue;
+
+    default:
+      val = atoi(card_name);
+      if ((val < 1) || (val > 10)) {
+        puts("I don't understand that value!");
+        continue;
+      }
+    }
+
+    if ((val > 2) && (val < 7)) {
+      count++;
+    } else if (val == 10) {
+      count--;
+    }
+    printf("Current count: %i\n", count);
+  }
+  return 0;
+}

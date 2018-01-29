b559715b5f32dd46c85629d6da47b026b0c730de
isa <isabel.blancett@students.olin.edu>
2018-01-28 19:24:20 -0500

    Rearranged cards code into more functions

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -1,16 +1,64 @@
+/*
+ * Author: Isabel Blancett
+ * Adapted from cards exercise in Head First C
+ * Software Systems Ex. 1
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
+
+
+/* Prompts the user for input and puts the reply in the given buffer.
+
+   User input is truncated to the first two characters.
+
+   prompt: string prompt to display
+   card_name: buffer where result is stored
+*/
+void get_card_name(char *prompt, char *card_name) {
+  puts(prompt);
+  scanf("%2s", card_name);
+}
+
+/* Calculates new count based on value
+
+   count: current counter
+   val: assigned card value
+   returns: count
+*/
+int update_counter(int count, int val) {
+  if ((val > 2) && (val < 7)) {
+    count++;
+  } else if (val == 10) {
+    count--;
+  }
+  return count;
+}
+
+/* Check if value is a card number
+
+   val: assigned card value
+*/
+void check_value(int val) {
+  if ((val < 1) || (val > 10)) {
+    puts("I don't understand that value!");
+  }
+}
 
 int main()
 {
 
   char card_name[3];
   int count = 0;
+
+  // pedantic option produces warnings as errors
+  // val declaration was moved out of the loop to avoid re-declaration
+  int val;
   while (card_name[0] != 'X') {
 
-    puts("Enter the card_name: ");
-    scanf("%2s", card_name);
-    int val = 0;
+    get_card_name("Enter the card_name: ", card_name);
+    val = 0;
 
     switch(card_name[0]) {
 
@@ -29,18 +77,12 @@ int main()
 
     default:
       val = atoi(card_name);
-      if ((val < 1) || (val > 10)) {
-        puts("I don't understand that value!");
-        continue;
-      }
+      check_value(val);
     }
 
-    if ((val > 2) && (val < 7)) {
-      count++;
-    } else if (val == 10) {
-      count--;
-    }
+    count = update_counter(count, val);
     printf("Current count: %i\n", count);
   }
   return 0;
+
 }

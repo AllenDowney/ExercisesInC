997f299e22501a62d95016f06d4bac1c4acff2c6
Allen Downey <downey@allendowney.com>
2018-04-02 14:09:43 -0400

    Adding newshound.c

---
+++ exercises/ex09/newshound.c
@@ -0,0 +1,52 @@
+/* Example from Head First C.
+
+Downloaded from https://github.com/twcamper/head-first-c
+
+Modified by Allen Downey.
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <wait.h>
+
+
+void error(char *msg)
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <search phrase>\n", argv[0]);
+        return 1;
+    }
+    const char *PYTHON = "/usr/bin/python2";
+    const char *SCRIPT = "rssgossip.py";
+    char *feeds[] = {
+        "http://www.nytimes.com/services/xml/rss/nyt/Africa.xml",
+        "http://www.nytimes.com/services/xml/rss/nyt/Americas.xml",
+        "http://www.nytimes.com/services/xml/rss/nyt/MiddleEast.xml",
+        "http://www.nytimes.com/services/xml/rss/nyt/Europe.xml",
+        "http://www.nytimes.com/services/xml/rss/nyt/AsiaPacific.xml"
+    };
+    int num_feeds = 5;
+    char *search_phrase = argv[1];
+    char var[255];
+
+    for (int i=0; i<num_feeds; i++) {
+        sprintf(var, "RSS_FEED=%s", feeds[i]);
+        char *vars[] = {var, NULL};
+
+        int res = execle(PYTHON, PYTHON, SCRIPT, search_phrase, NULL, vars);
+        if (res == -1) {
+            error("Can't run script.");
+        }
+    }
+    return 0;
+}

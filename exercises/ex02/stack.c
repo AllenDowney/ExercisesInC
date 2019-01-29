774db32b2f0c70211f3ae7a21b0111e7e17c7ec0
Allen Downey <downey@allendowney.com>
2019-01-29 16:18:26 -0500

    Uncommenting print statements

--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -14,7 +14,7 @@ int *foo() {
     int i;
     int array[SIZE];
 
-    //printf("%p\n", array);
+    printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = 42;
@@ -26,7 +26,7 @@ void bar() {
     int i;
     int array[SIZE];
 
-    //printf("%p\n", array);
+    printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = i;
